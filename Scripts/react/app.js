e287a43232cdf0b2d4f8a0fefc37abcb7fa1b217
cyrus <cyrus@coderepubliq.com>
2018-06-29 09:37:06 +0800

    initial commit

---
+++ Scripts/react/app.js
@@ -0,0 +1,58 @@
+﻿import React from "react";
+import ReactDOM from "react-dom";
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import Drawer from '@material-ui/core/Drawer';
+import Button from '@material-ui/core/Button';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+
+class App extends React.Component{
+
+    state = {
+        top: false,
+        left: false,
+        bottom: false,
+        right: false,
+    };
+
+    toggleDrawer = (side, open) => () => {
+        this.setState({
+            [side]: open,
+        });
+    };
+
+    render() {
+        return (
+            <div>
+                <AppBar position="static" color="default">
+                    <Toolbar>
+                        <IconButton color="inherit" aria-label="Menu">
+                            <MenuIcon />
+                        </IconButton>
+                        <Typography variant="title" color="inherit">
+                            Title
+                        </Typography>
+                    </Toolbar>
+                </AppBar>
+
+                <Button onClick={this.toggleDrawer('left', true)}>Open Left</Button>
+                <Drawer open={this.state.left} onClose={this.toggleDrawer('left', false)} >
+                    <div
+                        tabIndex={0}
+                        role="button"
+                        onClick={this.toggleDrawer('left', false)}
+                        onKeyDown={this.toggleDrawer('left', false)}
+                    >
+                        test
+                    </div>
+                </Drawer>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'))
